b3d23224bea765fed73adbd48ac8c32660f806a2
Mikołaj Szawerda <01169246@pw.edu.pl>
2022-05-22 22:43:41 +0200

    Added tests for reporting

--- api/tests/test_report.py
+++ api/tests/test_report.py
@@ -1,9 +1,12 @@
 from urllib import response
 from rest_framework import status
-from rest_framework.test import APITestCase
+from rest_framework.test import APITestCase, APIClient
 from django.urls import reverse
 import os
 import json
+import pytest
+# from parameterized import parameterized
+
 
 def json_correct_data():
     module_dir = os.path.dirname(__file__)
@@ -12,25 +15,43 @@ def json_correct_data():
         data = json.load(fh)
     return data
 
-def pay_by_link_proccesed():
+
+def pay_by_link_proccesed(file_name):
     module_dir = os.path.dirname(__file__)
-    file_path = os.path.join(module_dir, 'pay_by_link.json')
+    file_path = os.path.join(module_dir, f'{file_name}.json')
     with open(file_path, 'r') as fh:
         data = json.load(fh)
     return data
 
-class ReportAPITest(APITestCase):
 
-    def test_always_pass(self):
-        assert True
+@pytest.fixture
+def apiclient():
+    return APIClient()
 
-    def test_reporting_pay_by_link(self):
-        url = reverse('report')
-        pbl_data = json_correct_data()['pay_by_link']
-        response = self.client.post(url, {
-            'pay_by_link': pbl_data
+
+@pytest.mark.parametrize('report_type', ['pay_by_link', 'dp' , 'card'])
+def test_reporting_pay_by_link(apiclient, report_type):
+    url = reverse('report')
+    pbl_data = json_correct_data()[report_type]
+    response = apiclient.post(url, {
+        report_type: pbl_data
+    }, format="json")
+    assert response.status_code == status.HTTP_200_OK
+    correct_response = pay_by_link_proccesed(report_type)
+    assert response.json() == correct_response
+
+
+@pytest.mark.parametrize('report_type', ['pay_by_link', 'dp', 'card'])
+def test_reporting_pay_by_link_lack_of_field(apiclient, report_type):
+    url = reverse('report')
+    pbl_data = json_correct_data()[report_type]
+    keys = list(pbl_data[0].keys())
+    for key in keys:
+        for d in pbl_data:
+            d.pop(key)
+        response = apiclient.post(url, {
+            report_type: pbl_data
         }, format="json")
-        assert response.status_code == status.HTTP_200_OK
-        correct_response = pay_by_link_proccesed()
-        assert response.json() == correct_response
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+
 
--- api/views.py
+++ api/views.py
@@ -1,7 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import PayByLinkSerializer
+from .serializers import PayByLinkSerializer, DirectPaymentSerializer, CardSerializer
 from sortedcontainers import SortedList
 
 
@@ -17,13 +17,42 @@ def prepare_single_report(data, type, payment_mean):
         }
 
 
+def prepare_card_data(name, surname, number):
+    num = len(number)//2
+    stars = '*'*num
+    number = number[:num//2]+stars+number[3*num//2:]
+    return ' '.join([name, surname, number])
+
+
 @api_view(['POST'])
 def create_report(request):
-    pbl = request.data['pay_by_link']
-    parsed_data = PayByLinkSerializer(data=pbl, many=True)
-    if not parsed_data.is_valid():
-        return Response("Invalid data!", status=status.HTTP_400_BAD_REQUEST)
-    reports = SortedList(key=lambda x:x["date"])
-    for data in parsed_data.data:
-        reports.add(prepare_single_report(data, 'pay_by_link', data['bank']))
+    pbl = request.data.get('pay_by_link', None)
+    if pbl:
+        parsed_data = PayByLinkSerializer(data=pbl, many=True)
+        if not parsed_data.is_valid():
+            return Response("Invalid data!", status=status.HTTP_400_BAD_REQUEST)
+        reports = SortedList(key=lambda x:x["date"])
+        for data in parsed_data.data:
+            reports.add(prepare_single_report(data, 'pay_by_link', data['bank']))
+
+    dp = request.data.get('dp', None)
+    if dp:
+        parsed_data = DirectPaymentSerializer(data=dp, many=True)
+        if not parsed_data.is_valid():
+            return Response("Invalid data!", status=status.HTTP_400_BAD_REQUEST)
+        reports = SortedList(key=lambda x:x["date"])
+        for data in parsed_data.data:
+            reports.add(prepare_single_report(data, 'dp', data['iban']))
+
+    card = request.data.get('card', None)
+    if card:
+        parsed_data = CardSerializer(data=card, many=True)
+        if not parsed_data.is_valid():
+            return Response("Invalid data!", status=status.HTTP_400_BAD_REQUEST)
+        reports = SortedList(key=lambda x:x["date"])
+        for data in parsed_data.data:
+            name = data['cardholder_name']
+            surname = data['cardholder_surname']
+            number = data['card_number']
+            reports.add(prepare_single_report(data, 'card', prepare_card_data(name, surname, number)))
     return Response(reports.__iter__())
